dc019871b7055ad59f6412ba0e2bb7b4a28f018d
aonor <justsomechannel261@gmail.com>
2020-10-26 20:55:18 -0500

    added nth root, working on log currently, needs fixing

--- calculator.py
+++ calculator.py
@@ -1,66 +1,74 @@
-import math 
+import math
 
-#A
 def add(x, y):
     return x + y
-#B
+
 def subtract(x, y):
     return x - y
-#C
+
 def multiply(x, y):
     return x * y
-#D
+
 def divide(x, y):
     return x / y
-#E
+
 def exponent(x, y):
     return (x ** y)
-#F
+
 def sqrt(x):
     return(x)**(1/2)
 
-pi = math.pi
+def nthroot(x, y):
+    return pow(y,(1/x))
+
+def log(x, y):
+    return log(x)/log(y)
 
 
 print("Select an operation:\n")
-print("   A: addition\n   B: subtraction\n   C: multiplication\n   D: division\n   E: exponents\n   F: square root\n")
+print("   addition\n   subtraction\n   multiplication\n   division\n   exponents\n   square root\n   nth root\n   logarithm\n")
 
 
 while True:
     # User imput
-    choice = input("Enter operation's code letter: ")
+    choice = input("Enter operation's name: ")
 
     # Check if choice is one of the options
-    if choice in ("A", "B", "C", "D", "E"):
+    if choice in ("addition", "subtraction", "multiplication", "division", "exponents", "nth root", "logarithm"):
         x = float(input("Enter first number: "))
         y = float(input("Enter second number: "))
 
-        if choice == "A":
+        if choice == "addition":
             print(x, "+", y, "=", add(x, y))
 
-        elif choice == "B":
+        elif choice == "subtraction":
             print(x, "-", y, "=", subtract(x, y))
 
-        elif choice == "C":
+        elif choice == "multiplication":
             print(x, "*", y, "=", multiply(x, y))
 
-        elif choice == "D":
+        elif choice == "division":
             print(x, "/", y, "=", divide(x, y))
 
-        elif choice == "E":
+        elif choice == "exponents":
             print(x, "^", y, "=", exponent(x, y))
 
-            break
+        elif choice == "nth root":
+            print(x, "^√", y, "=", nthroot(x, y))
+
+        elif choice == "logarithm":
+            print("log", "(", x, "[", " ", y, "]", ")", "=", log(x, y))
+
         else:
             print("Invalid Input")
 
 
-    if choice in ("F"):
+    if choice in ("square root"):
         x = float(input("Enter first number: "))
 
-        if choice == "F":
+        if choice == "square root":
             print("√", x, "=", sqrt(x))
 
-            break
+
         else:
             print("Invalid Input")
